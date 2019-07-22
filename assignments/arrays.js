baa9771c9318b71581d1801e5f0156649e667e36
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2019-07-22 17:28:16 -0500

    last commit

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -63,6 +63,7 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 
 // ==== Challenge 1 ====
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
+
 console.log('Car 33 is a ' + inventory[32].car_year + " " + inventory[32].car_make + " " + inventory[32].car_model);
 
 // console.log(`Car 33 is a *car year goes here* *car make goes here* *car model goes here*` );
@@ -77,7 +78,7 @@ console.log(lastCar);
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
 for(let i = 0; i < inventory.length; i++){
-    carModels.unshift(inventory[i].car_model)
+    carModels.push(inventory[i].car_model)
 }
 carModels.sort()
 console.log(carModels);
@@ -104,7 +105,7 @@ console.log(oldCars);
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi = [];
     for(let i = 0; i < inventory.length; i++){
-        if(inventory[i].car_make === "BMW" || inventory[i].car_make ==="Audi" ){
+        if(inventory[i].car_make === "BMW" || inventory[i].car_make === "Audi" ){
             BMWAndAudi.push(inventory[i])
         }
     }
