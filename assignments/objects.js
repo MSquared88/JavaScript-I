0098163bc223e04df480ae05e2eec05b970e4c34
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2019-07-22 16:30:02 -0500

    fixed problem with kenan speak method

--- assignments/objects.js
+++ assignments/objects.js
@@ -77,7 +77,7 @@ console.log(antonietta.gender)
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 
-console.log(kennan.speak());
+kennan.speak();
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 
